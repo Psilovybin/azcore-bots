c704f0292849b9727f3127260d7a6a9d1ed0ef2d
Psylovybin <me@chrisfaulkner.org>
2019-07-19 22:35:46 -0500

    Adding

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CUR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PATH_MODULES="$CUR_PATH/modules/"
+[ ! -d $PATH_MODULES/drassil/joiner ] && git clone https://github.com/drassil/joiner $PATH_MODULES/drassil/joiner -b master
+source "$PATH_MODULES/drassil/joiner/joiner.sh"
+
+
+if [[ $1 == "dev" ]]; then
+    git submodule update --init "$CUR_PATH/data/doc"
+fi
